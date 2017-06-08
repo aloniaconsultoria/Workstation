e25de4bcec80d53db8b740eb1335ac1749a853a0
chef <chef@centos-6.(none)>
2017-06-08 19:40:50 +0200

    Nuevas modificaciones dia 8

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name 'workstation'
 maintainer 'The Authors'
-maintainer_email 'you@example.com'
+maintainer_email 'agarcia@aloniaconsultoria.com'
 license 'all_rights'
 description 'Installs/Configures workstation'
 long_description 'Installs/Configures workstation'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,4 +3,4 @@
 # Recipe:: default
 #
 # Copyright (c) 2017 The Authors, All Rights Reserved.
-include_receipe "workstation::setup"
+include_recipe "workstation::setup"
